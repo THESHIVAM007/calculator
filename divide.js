8c6d97c9c78171971ec10e7c44d0c4fd50bcb5ec
SHIVAM <sciit2003@gmail.com>
2022-03-02 20:57:37 +0530

    commit

---
+++ divide.js
@@ -0,0 +1,4 @@
+function divide (a,b){
+return (a/b).toFixed(3)
+}
+module.exports = divide
---
+++ main.js
@@ -0,0 +1,10 @@
+const ADD = require("./add")
+const SUBTRACT = require("./subtract")
+const MULTIPLY = require("./multiply")
+const DIVIDE = require("./divide")
+
+
+console.log("Sum :",ADD(12,5))
+console.log("Difference:",SUBTRACT(12,5))
+console.log("Product:",MULTIPLY(12,5))
+console.log("quotient:" ,DIVIDE(12,5))
